e6bcac99e7d7f84740e27cb0c0c8da4f2b99f10c
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 02:48:39 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,4 @@
 import mlflow
-import os
-
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,34 +7,38 @@ from sklearn.metrics import accuracy_score
 # Use local MLflow storage
 mlflow.set_tracking_uri("file:./mlruns")
 
-# Load data
+# Load dataset
 data = load_iris()
 X = data.data
 y = data.target
 
-# Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# make training deterministic
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Start MLflow run
 with mlflow.start_run() as run:
 
-    model = RandomForestClassifier(n_estimators=200)
+    # deterministic model
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
 
+    # Predictions
     y_pred = model.predict(X_test)
 
+    # Accuracy
     accuracy = accuracy_score(y_test, y_pred)
-
     print("Accuracy:", accuracy)
 
-    # Log accuracy to MLflow
+    # Log to MLflow
     mlflow.log_metric("accuracy", accuracy)
 
     # Get run ID
     run_id = run.info.run_id
 
-    # Save run ID
+    # Save run ID (IMPORTANT for pipeline)
     with open("model_info.txt", "w") as f:
         f.write(run_id)
 
-print("Training completed")
+print("Training completed successfully")
