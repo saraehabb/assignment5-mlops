4379eaa5fda9a523532056034433578ee2f1fc72
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 02:07:18 -0400

    Update check_threshold.py

--- check_threshold.py
+++ check_threshold.py
@@ -1,19 +1,8 @@
-import mlflow
-import os
-
-mlflow.set_tracking_uri("file:./mlruns")
-
-with open("model_info.txt", "r") as f:
-    run_id = f.read().strip()
-
-client = mlflow.tracking.MlflowClient()
-run = client.get_run(run_id)
-
-accuracy = run.data.metrics["accuracy"]
+with open("accuracy.txt", "r") as f:
+    accuracy = float(f.read())
 
 print("Accuracy:", accuracy)
 
-# DO NOT FAIL PIPELINE
 if accuracy < 0.85:
     print("Accuracy below threshold, but continuing...")
 
