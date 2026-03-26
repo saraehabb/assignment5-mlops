0edc141d7311ed06dba114b5e07072666b663987
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 02:34:34 -0400

    Update train.py

--- train.py
+++ train.py
@@ -15,12 +15,12 @@ X = data.data
 y = data.target
 
 # Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Start MLflow run
 with mlflow.start_run() as run:
 
-    model = RandomForestClassifier()
+    model = RandomForestClassifier(n_estimators=200)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
