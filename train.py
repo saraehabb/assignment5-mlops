f3abb7a05b49297c77631668db5ad79d115f0842
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 02:52:16 -0400

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 with mlflow.start_run() as run:
 
     # deterministic model
-    model = RandomForestClassifier(n_estimators=200, random_state=42)
+    model = RandomForestClassifier(
+    n_estimators=500,
+    max_depth=10,
+    random_state=42
+)
     model.fit(X_train, y_train)
 
     # Predictions
