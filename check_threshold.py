1bd53799bca98041d8e8a76f928ec9c081e8a8cf
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 03:13:31 -0400

    Update check_threshold.py

--- check_threshold.py
+++ check_threshold.py
@@ -1,19 +1,28 @@
+# FIRST try MLflow
 import mlflow
 
-# point to downloaded mlruns
-mlflow.set_tracking_uri("file:./mlruns")
+try:
+    mlflow.set_tracking_uri("file:./mlruns")
 
-# Read run_id
-with open("model_info.txt", "r") as f:
-    run_id = f.read().strip()
+    with open("model_info.txt", "r") as f:
+        run_id = f.read().strip()
 
-client = mlflow.tracking.MlflowClient()
-run = client.get_run(run_id)
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(run_id)
 
-accuracy = run.data.metrics["accuracy"]
+    accuracy = run.data.metrics["accuracy"]
 
-print("Accuracy:", accuracy)
+    print("Accuracy from MLflow:", accuracy)
 
+except:
+    print("MLflow failed → fallback to file")
+
+    with open("accuracy.txt", "r") as f:
+        accuracy = float(f.read())
+
+    print("Accuracy from file:", accuracy)
+
+# FINAL DECISION
 if accuracy < 0.85:
     print("Accuracy below threshold → FAIL")
     exit(1)
