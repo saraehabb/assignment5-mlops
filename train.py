3330406da09298d476d9e26661f8861054ac9fb3
saraehabb <154023303+saraehabb@users.noreply.github.com>
2026-03-26 01:34:42 -0400

    Add train.py

---
+++ train.py
@@ -0,0 +1,31 @@
+import mlflow
+import os
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI"))
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+with mlflow.start_run() as run:
+
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", accuracy)
+
+    run_id = run.info.run_id
+
+    with open("model_info.txt", "w") as f:
+        f.write(run_id)
